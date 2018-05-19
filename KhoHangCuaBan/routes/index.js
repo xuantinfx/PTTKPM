b749c86e639251383e029d9986ce7546762a5bea
xuantinfx <xuantinfx@gmail.com>
2018-05-19 11:22:13 +0700

    thiết kế giao diện hoàn tất + hbs

--- KhoHangCuaBan/routes/index.js
+++ KhoHangCuaBan/routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/:id', function(req, res, next) {
+  res.render(req.params.id, { title: 'Express' });
 });
 
 module.exports = router;
