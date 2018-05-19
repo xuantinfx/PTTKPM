8ded844a040e9910ecd0c5d286b32206ec20f71b
xuantinfx <xuantinfx@gmail.com>
2018-05-19 21:18:57 +0700

    thêm connectionDB vào req

--- KhoHangCuaBan/app.js
+++ KhoHangCuaBan/app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var mysql = require('mysql');
+var config = require('./config')
+
 
 var app = express();
 
@@ -15,20 +18,36 @@ app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Dùng middlewere này để chèn những thứ cần thiết vào req
+app.use((req, res, next) => {
+  //Chèn connectionDB vào để sử dụng để query DB
+  let connection = mysql.createConnection({
+    host: config.mysql.host,
+    user: config.mysql.user,
+    password: config.mysql.pass,
+    database: config.mysql.database,
+    port: config.mysql.port
+  });
+  req.connectionToDB = connection;
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
